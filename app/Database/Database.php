beb975935bf68c3a01ceb858097528907784d791
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2025-12-27 00:58:36 +0100

    code de la page Database complet

---
+++ app/Database/Database.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Database;
+
+use PDO;
+
+class Database {
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
+        $this->pdo = new PDO(
+            "mysql:host=localhost;dbname=project_manager;charset=utf8",
+            "root",
+            "",
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
